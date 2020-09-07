c3da3f6367ba08dc908cd31dd516b701c722c138
Aleksandra Spadlo <alexandra619@gmail.com>
2020-09-07 12:40:34 +0100

    Creating a demo

---
+++ precache-manifest.4db2be49a5add41fd9b1d713c397d685.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4273dfe029f8b0fb6e44c2a91e30f610",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d05103076fd08d0b14c",
+    "url": "./static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "36d3d6b799ff7a7305d0",
+    "url": "./static/css/main.0bbd6276.chunk.css"
+  },
+  {
+    "revision": "5d05103076fd08d0b14c",
+    "url": "./static/js/2.5f44b352.chunk.js"
+  },
+  {
+    "revision": "50056c67caf144a398da2c4c8754b04d",
+    "url": "./static/js/2.5f44b352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d3d6b799ff7a7305d0",
+    "url": "./static/js/main.7f952ba1.chunk.js"
+  },
+  {
+    "revision": "a1184acbdc8829664c4c",
+    "url": "./static/js/runtime-main.cb8b83d1.js"
+  },
+  {
+    "revision": "f23d9f2818b4732cc005b6dceeced5eb",
+    "url": "./static/media/ops.f23d9f28.png"
+  },
+  {
+    "revision": "a829e0f9a4eacc53ba832b1a94d50a6d",
+    "url": "./static/media/tictoctoe.a829e0f9.png"
+  },
+  {
+    "revision": "844bf2dc1edb7f4472f11b5b72f5fe11",
+    "url": "./static/media/winner.844bf2dc.png"
+  }
+]);
